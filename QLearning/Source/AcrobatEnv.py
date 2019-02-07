b394cab635f16c0165f0fb4c492ee3bc92024dad
ErickStone <erick.stone.cs@gmail.com>
2019-02-06 17:00:56 -0700

    aseddf

--- QLearning/Source/AcrobatEnv.py
+++ QLearning/Source/AcrobatEnv.py
@@ -15,9 +15,11 @@ def get_state(s, bins):
 
 
 def get_reward(s, s_n, r, done):
+    # finished episodes count against forcing progression
+    # big rewards for being above the half way mark
     if done and s_n < .5:
         return -10
-    elif done and s_n > .5:
+    elif s_n > .5:
         return 10
     # down is pi/2 this means that up which is desired is 3 pi/2
     # increase if clock wise of previous state
@@ -33,7 +35,7 @@ def get_reward(s, s_n, r, done):
 # min_ts, max_ts, bins, train_until,
 # success_count, r_clo
 success_table, episode_table = QLearning.q_learning(gym.make("Acrobot-v1"), 50000, .1, 1,
-                     0, 2000, 1, 3000,
+                     0, 2000, 1, 6000,
                      15, get_reward, get_state)
 
 plt.plot(success_table, episode_table)
